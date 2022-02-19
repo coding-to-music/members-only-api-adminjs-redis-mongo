180d8b530e9876ed006eb6cb095e91cf99a4f4d8
Lekan Adetunmbi <lothbroch@gmail.com>
2022-02-19 02:55:26 +0100

    Fixed app crash issue due to passport method import

--- routes/api/postRouter.ts
+++ routes/api/postRouter.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
 import { CustomIRouter } from "@interfaces/routes.interface";
-import { authenticate } from "passport";
+import passport from "passport";
 import { get_all_post_by_user, get_get_all_posts, get_get_post_by_id, post_create_post, put_add_comments, put_add_likes } from "@controllers/postController";
 
 
 const postRouter: CustomIRouter = Router();
 
-postRouter.get('/all_posts', authenticate('jwt', { session: false }), get_get_all_posts);
+postRouter.get('/all_posts', passport.authenticate('jwt', { session: false }), get_get_all_posts);
 
-postRouter.get('/:id', authenticate('jwt', { session: false }), get_get_post_by_id);
+postRouter.get('/:id', passport.authenticate('jwt', { session: false }), get_get_post_by_id);
 
-postRouter.get('/post_by_user', authenticate('jwt', { session: false }), get_all_post_by_user);
+postRouter.get('/post_by_user', passport.authenticate('jwt', { session: false }), get_all_post_by_user);
 
-postRouter.post('/create_post', authenticate('jwt', { session: false }), post_create_post);
+postRouter.post('/create_post', passport.authenticate('jwt', { session: false }), post_create_post);
 
-postRouter.put('/:id/add_comment', authenticate('jwt', { session: false }), put_add_comments);
+postRouter.put('/:id/add_comment', passport.authenticate('jwt', { session: false }), put_add_comments);
 
-postRouter.put('/:id/add_like', authenticate('jwt', { session: false }), put_add_likes);
+postRouter.put('/:id/add_like', passport.authenticate('jwt', { session: false }), put_add_likes);
 
 export default postRouter;
--- routes/api/userRouter.ts
+++ routes/api/userRouter.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { authenticate } from "passport";
+import passport from "passport";
 import { CustomIRouter } from "@interfaces/routes.interface";
 import { get_get_user, post_create_user } from "@controllers/userController";
 import { authorize_user } from "@controllers/authController";
 
 const userRouter: CustomIRouter = Router();
 
-userRouter.get('/userinfo', authenticate('jwt', { session: false }), authorize_user, get_get_user);
+userRouter.get('/userinfo', passport.authenticate('jwt', { session: false }), authorize_user, get_get_user);
 
 userRouter.post('/register', post_create_user);
 
