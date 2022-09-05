b23304a77656b227d1e31085fe7de70495f20c31
Lekan Adetunmbi <lothbroch@gmail.com>
2022-09-05 09:46:06 +0100

    Fixed Test Suite and All Currently defined tests passed

--- src/routes/api/api.ts
+++ src/routes/api/api.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
-import authRouter from '@src/routes/api/authRouter';
-import mediaRouter from '@src/routes/api/mediaRouter';
-import messageRouter from '@src/routes/api/messageRouter';
-import passwordRouter from '@src/routes/api/passwordRouter';
+import authRouter from '@routes/api/authRouter';
+import mediaRouter from '@routes/api/mediaRouter';
+import messageRouter from '@routes/api/messageRouter';
+import passwordRouter from '@routes/api/passwordRouter';
 import postRouter from '@routes/api/postRouter';
-import profileRouter from '@src/routes/api/profileRouter';
-import userRouter from '@src/routes/api/userRouter';
+import profileRouter from '@routes/api/profileRouter';
+import userRouter from '@routes/api/userRouter';
 
 const apiRouter: Router = Router();
 
 apiRouter.get('/', (req, res) => res.json({
-    message: 'HELLO VISITOR, THANK YOU FOR STOPPING-BY AND WELCOME TO MEMBERS-ONLY API',
-    documentation: `Please visit '/api-docs' for API Documentation`
+    message: `HELLO VISITOR, THANK YOU FOR STOPPING-BY AND WELCOME TO MEMBERS-ONLY API.
+    PLEASE VISIT '/api-docs' FOR FULL API DOCUMENTATION`
 }));
 
 apiRouter.use('/auth', authRouter);
